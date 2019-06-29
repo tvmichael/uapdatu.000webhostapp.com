ee56974bfad0b1552692c1aa63786b868d508d1c
tvmichael <tvmichael@meta.ua>
2019-06-29 23:16:53 +0300

    base file

---
+++ basic/views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this yii\web\View */
+
+use yii\helpers\Url;
+
+$this->title = 'PDATU';
+?>
+<div class="site-index">
+    <div class="jumbotron">
+        <h2>Подільський державний аграрно-технічний університет</h2>
+        <p class="lead">Навчально-науковий інститут дистанційної освіти.</p>
+        <p><a class="btn btn-xs btn-success" href="<?=Url::to(['/site/login']);?>">Увійти на сайт</a></p>
+    </div>
+    <div class="body-content">
+    </div>
+</div>
